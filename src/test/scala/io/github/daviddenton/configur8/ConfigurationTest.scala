7f77f472bc1764fac347b58ee8432458f55760c2
david denton <david.denton@springer.com>
2015-02-07 10:44:39 +0000

    dave - using custom serialization and deserialization

--- src/test/scala/io/github/daviddenton/configur8/ConfigurationTest.scala
+++ src/test/scala/io/github/daviddenton/configur8/ConfigurationTest.scala
@@ -1,5 +1,7 @@
 package io.github.daviddenton.configur8
 
+import java.lang.Integer._
+
 import io.github.daviddenton.configur8.Configuration.ConfigurationBuilder
 import io.github.daviddenton.configur8.Property._
 import org.scalatest.{FunSpec, ShouldMatchers}
@@ -8,8 +10,16 @@ class ConfigurationTest extends FunSpec with ShouldMatchers {
 
   private val intProperty = integer("anInteger")
 
-  private case class UserType(value: String) {
+  case class SimpleWrapperType(value: String) {
     override def toString: String = value
+  }
+
+  case class CustomSerializedType(value: Int) {
+    def describe = "!!!" + value + "!!!"
+  }
+
+  object CustomSerializedType {
+    def apply(value: String): CustomSerializedType = CustomSerializedType(parseInt(value.replace("!", "")))
   }
 
   describe("Configuration") {
@@ -33,11 +43,6 @@ class ConfigurationTest extends FunSpec with ShouldMatchers {
         }
       }
 
-      it("can define properties with user defined type") {
-        val userTypeProperty = Property.of("userDefined", UserType)
-        ConfigurationBuilder().withProp(userTypeProperty, UserType("bob")).build.valueOf(userTypeProperty) should be === UserType("bob")
-      }
-
       it("uses default value") {
         ConfigurationBuilder().withProp(intProperty, 999).build.valueOf(intProperty) should be === 999
       }
@@ -51,6 +56,16 @@ class ConfigurationTest extends FunSpec with ShouldMatchers {
         val property = string("A_PROP")
         System.setProperty(property.name, "value")
         ConfigurationBuilder().withProp(property, "default").build.valueOf(property) should be === System.getProperty(property.name)
+      }
+
+      it("can define properties with user defined type") {
+        val property = Property.of("simpleWrapper", SimpleWrapperType)
+        ConfigurationBuilder().withProp(property, SimpleWrapperType("bob")).build.valueOf(property) should be === SimpleWrapperType("bob")
+      }
+
+      it("can define properties with a type that uses custom serialization and deserialization") {
+        val property = Property.of[CustomSerializedType]("customSerialize", CustomSerializedType(_), c => c.describe)
+        ConfigurationBuilder().withProp(property, CustomSerializedType("!!!5!!!")).build.valueOf(property) should be === CustomSerializedType("!!!5!!!")
       }
     }
   }
