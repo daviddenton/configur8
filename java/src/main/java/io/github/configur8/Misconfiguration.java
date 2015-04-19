f2c8f1b997f37592826d5b0eb66f945c12b4b169
david denton <david.denton@springer.com>
2015-04-19 19:50:09 +0100

    dave - started porting scala version to java

--- java/src/main/java/io/github/configur8/Misconfiguration.java
+++ java/src/main/java/io/github/configur8/Misconfiguration.java
@@ -1,11 +1,6 @@
 package io.github.configur8;
 
 public class Misconfiguration extends RuntimeException {
-
-    public static void throwUp(String message, String name) {
-        throw new Misconfiguration(message + " '" + name + "'");
-    }
-
     public Misconfiguration(String message) {
         super(message);
     }
