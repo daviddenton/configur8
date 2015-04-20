60664e949b93d13e9e28001819be1a25bd88514a
david denton <david.denton@springer.com>
2015-04-20 12:10:21 +0100

    dave - added publish.sh to global mega build

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Releasing and publishing v$1
+
+git tag -a $1 -m "released version $1 to bintray"
+git tag
+git push origin $1
+
+cd java
+./publish $1
+
+cd ..
+
+cd scala
+./publish.sh $1
+
